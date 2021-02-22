5312d1bfcf11154095e308467de75af5e1a6380f
DrSavauG <dr.savaug@gmail.com>
2021-02-22 12:56:43 +0300

    foreach

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 module.exports = function towelSort(matrix) {
-    if (!matrix) return [];
-    return matrix
-        .reduce((sum, el, i) => {
+    let arr = [];
+    if (matrix)
+        matrix.forEach((el, i) => {
             if (i % 2 !== 0) el.reverse();
-            sum.push(el);
-            return sum;
-        }, [])
-        .flat();
+            arr.push(el);
+        });
+    return arr.flat();
 };
