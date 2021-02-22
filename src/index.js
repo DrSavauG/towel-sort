aeb48a8f0f3d54341c359c970b70f1f32ab19789
DrSavauG <dr.savaug@gmail.com>
2021-02-22 12:17:56 +0300

    d

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function towelSort(matrix) {
-    return [];
+    if (!matrix) return [];
+    
+    let arr = [];
+    for (let i = 0; i < matrix.length; i++) {
+        i % 2 === 0 ? arr.push(matrix[i]) : arr.push(matrix[i].reverse());
+    }
+    return arr.flat();
 };
