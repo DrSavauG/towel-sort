01e2503cb54081587829032bf6c8aa6b226d24ba
DrSavauG <dr.savaug@gmail.com>
2021-02-22 12:37:27 +0300

    with reduce

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 module.exports = function towelSort(matrix) {
     if (!matrix) return [];
-    
-    let arr = [];
-    for (let i = 0; i < matrix.length; i++) {
-        i % 2 === 0 ? arr.push(matrix[i]) : arr.push(matrix[i].reverse());
-    }
-    return arr.flat();
+    return matrix
+        .reduce((sum, el, i) => {
+            if (i % 2 !== 0) el.reverse();
+            sum.push(el);
+            return sum;
+        }, [])
+        .flat();
 };
